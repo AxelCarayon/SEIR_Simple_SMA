c95c6d88b082b35c7cc7b245d264ff1106f398c6
AxelCarayon <axel.carayon@gmail.com>
2022-04-19 16:15:07 +0200

    improved asynchronous mode

--- src/main/java/sma/scheduler/FairAsynchronousScheduler.java
+++ src/main/java/sma/scheduler/FairAsynchronousScheduler.java
@@ -4,15 +4,36 @@ import sma.agents.Agent;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FairAsynchronousScheduler implements Scheduler{
 
-    private List<Agent> agents;
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+    private Queue<Agent> queue;
 
     public FairAsynchronousScheduler(Agent[] agents) {
-        this.agents = Arrays.asList(agents);
+        this.queue = new ConcurrentLinkedQueue<>(Arrays.stream(agents).toList());
     }
 
     public void nextCycle() {
-        agents.stream().parallel().forEach(a -> a.wakeUp());
+
+        List<Future<Agent>> results = queue.parallelStream().map(agent -> executor.submit(() -> {agent.wakeUp(); return agent;})).collect(Collectors.toList());
+        Function<Future<Agent>,Agent> futureTreatment = futureAgent -> {
+            try {
+                return futureAgent.get();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        };
+
+        List<Agent> nextQueue = results.parallelStream().map(futureTreatment).collect(Collectors.toList());
+        queue = new ConcurrentLinkedQueue<>(nextQueue);
     }
+
 }
