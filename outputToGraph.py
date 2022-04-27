5130f6f9410f283753edfd6592c179e380df1dcc
AxelCarayon <axel.carayon@gmail.com>
2022-04-27 13:25:36 +0200

    fix code type

--- outputToGraph.py
+++ outputToGraph.py
@@ -7,64 +7,73 @@ OUTPUT_FOLDER = 'src/main/resources/pythonOutput'
 JAR_LOCATION = 'out/artifacts/SMA_SEIR_jar/SMA-SEIR.jar'
 YAML_FILE = 'src/main/resources/parameters.yaml'
 
-def readCSV(fileName):
-    with open(fileName, 'r') as csvfile:
+
+def read_csv(filename):
+    with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile)
         return list(reader)
 
-def getValues() :
+
+def get_values():
     with open(YAML_FILE, 'r') as file:
         data = yaml.safe_load(file)
         incubation = data['incubationRate']
         infection = data['infectionRate']
         recovery = data['recoveryRate']
-        looseImmunity = data['looseImmunityRate']
-    return f"incubationRate : {incubation} InfectionRate : {infection}\n RecoveryRate : {recovery} LooseImmunityRate : {looseImmunity}"
+        loose_immunity = data['looseImmunityRate']
+    return f"incubationRate : {incubation} InfectionRate : {infection}\n " \
+           f"RecoveryRate : {recovery} LooseImmunityRate : {loose_immunity}"
 
-def makeDiagram(fileName):
-    data = readCSV(fileName)
+
+def make_diagram(filename):
+    data = read_csv(filename)
     suceptible = []
     exposed = []
-    recovred = []
+    recovered = []
     infected = []
 
     for row in data[1:]:
         suceptible.append(int(row[0]))
         exposed.append(int(row[1]))
-        recovred.append(int(row[2]))
+        recovered.append(int(row[2]))
         infected.append(int(row[3]))
-    
-    plt.title(getValues())
+
+    plt.title(get_values())
     plt.plot(suceptible, label='Suceptible', color='gray')
     plt.plot(exposed, label='Exposed', color='yellow')
     plt.plot(infected, label='Infected', color='red')
-    plt.plot(recovred, label='Recovered', color='green')
+    plt.plot(recovered, label='Recovered', color='green')
     plt.xlabel('Cycles')
     plt.ylabel('Peoples')
     plt.legend()
-    plt.savefig(f'{fileName.split(".")[0]}.png')
-    #plt.show()
+    plt.savefig(f'{filename.split(".")[0]}.png')
+    # plt.show()
     plt.close()
 
-def runJavaJar(fileName):
-    import subprocess
-    subprocess.call(['java', '-jar', fileName])
 
-def copyToOutputFolder(fileName):
+def run_java_jar(filename):
+    import subprocess
+    subprocess.call(['java', '-jar', filename])
+
+
+def copy_to_output_folder(filename):
     import shutil
-    shutil.copy(OUTPUT_FILE_LOCATION, f"{OUTPUT_FOLDER}/{fileName}")
-    
-def createFile():
+    shutil.copy(OUTPUT_FILE_LOCATION, f"{OUTPUT_FOLDER}/{filename}")
+
+
+def create_file():
     with open(OUTPUT_FILE_LOCATION, 'w') as file:
         file.write('')
 
-def editYaml(key,value):
+
+def edit_yaml(key, value):
     with open(YAML_FILE, 'r') as file:
         data = yaml.safe_load(file)
     data[key] = value
-    
+
     with open(YAML_FILE, 'w') as file:
         yaml.dump(data, file)
+
 
 # if __name__ == "__main__":
 #     for i in range(10):
@@ -73,5 +82,5 @@ def editYaml(key,value):
 #         copyToOutputFolder(f"output{i}.csv")
 #         makeDiagram(f"{OUTPUT_FOLDER}/output{i}.csv")
 
-runJavaJar(JAR_LOCATION)
-makeDiagram(OUTPUT_FILE_LOCATION)
+run_java_jar(JAR_LOCATION)
+make_diagram(OUTPUT_FILE_LOCATION)
